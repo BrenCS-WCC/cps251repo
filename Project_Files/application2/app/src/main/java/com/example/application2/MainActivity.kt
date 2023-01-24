6ccea6fea0800938e45a17dd40258fca54866837
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-01-24 18:45:45 -0500

    Updated assignment2

--- Project_Files/application2/app/src/main/java/com/example/application2/MainActivity.kt
+++ Project_Files/application2/app/src/main/java/com/example/application2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.application2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.application2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
