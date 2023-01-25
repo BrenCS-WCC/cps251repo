dac1f84991b33b2de657656ea4f00b9561caef2a
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-01-24 19:08:12 -0500

    Updated assignment2

--- Project_Files/application2/app/src/main/java/com/example/application2/MainActivity.kt
+++ Project_Files/application2/app/src/main/java/com/example/application2/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.application2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.application2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private val TAG = "CONLOG" // Indicate log source as main project
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.i(TAG,"Created new activity " + this.localClassName)
     }
 }
