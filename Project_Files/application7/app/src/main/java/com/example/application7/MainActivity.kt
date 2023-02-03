418bbbbe906475e72131bad9b6569fcf5d473283
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-02-02 19:54:28 -0500

    Created application7

---
+++ Project_Files/application7/app/src/main/java/com/example/application7/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.application7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+
+import com.example.application7.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
