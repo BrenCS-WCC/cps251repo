e1212d777f41a7c09e320d94b195a86f07c3d222
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-02-13 19:19:34 -0500

    Built assignment 3

--- Project_Files/assignment3/app/src/main/java/com/example/assignment3/MainActivity.kt
+++ Project_Files/assignment3/app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.assignment3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.assignment3.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -12,5 +13,46 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val statusLabel = binding.statusLabel
+        val reportLabel = binding.calcReportLabel
+
+        val moneyInput = binding.numEntry
+
+        val calcButton = binding.calcTipActor
+
+        calcButton.setOnClickListener {
+            //statusLabel.text = "Clicked!"
+
+            // Reset displayed strings
+            statusLabel.text = ""
+            reportLabel.text = ""
+
+            if (moneyInput.text.toString().isBlank()) {
+                statusLabel.setText(R.string.blankInputError)
+            } else {
+                statusLabel.setText(R.string.readyStatusMsg)
+
+                val billTotal : Double = moneyInput.text.toString().toDouble()
+                val percents = arrayOf(
+                    0.10,
+                    0.15,
+                    0.20,
+                    0.25
+                )
+
+                var summaryString = ""
+
+                for (i in percents) {
+                    var value = i
+                    value = (value * billTotal) + billTotal
+
+                    val stringValue = String.format(Locale.US, "%.2f", value)
+                    summaryString = summaryString + (i * 100).toInt().toString() + "% = " + stringValue + "\n"
+                }
+
+                reportLabel.text = summaryString
+                }
+            }
+        }
     }
-}
