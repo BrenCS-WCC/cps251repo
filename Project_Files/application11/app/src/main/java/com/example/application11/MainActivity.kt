6666260982b627d7b00ea65c89ff521520946ab6
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-03-02 21:07:50 -0500

    Created application11: DataBinding

---
+++ Project_Files/application11/app/src/main/java/com/example/application11/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.application11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.application11.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
