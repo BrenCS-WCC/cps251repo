d0e7801c25bc989488f6ee25887ce0b8fa1e0102
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-03-02 21:51:14 -0500

    Added application 12: Observing lifecycle events

---
+++ Project_Files/application12/app/src/main/java/com/example/application12/DemoObserver.kt
@@ -0,0 +1,16 @@
+package com.example.application12
+
+import android.util.Log
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.LifecycleOwner
+
+class DemoObserver {
+    private val LOGTAG = "DemoObserver"
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    fun onAny(owner: LifecycleOwner, event: Lifecycle.Event) {
+        Log.i(LOGTAG, owner.lifecycle.currentState.name)
+    }
+}
