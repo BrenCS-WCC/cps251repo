8ae17631481803b0b2e02d6e26b09d0a581ba01b
Bren Straub <brstraub@wccnet.edu>
2023-03-04 00:21:12 -0500

    Updated assignment 5

--- Project_Files/assignment5/app/src/main/java/com/example/assignment5/ui/main/MainFragment.kt
+++ Project_Files/assignment5/app/src/main/java/com/example/assignment5/ui/main/MainFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 
 import com.example.assignment5.databinding.FragmentMainBinding
 
