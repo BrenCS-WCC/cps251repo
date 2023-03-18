9891b34b0c79a4039b69822272ae7cc830be1132
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-03-17 20:33:39 -0400

    Updated application14

---
+++ Project_Files/application14/app/src/main/java/com/example/application14/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.application14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.application14.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
