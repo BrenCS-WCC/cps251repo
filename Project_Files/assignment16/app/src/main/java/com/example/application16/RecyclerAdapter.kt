aabfa47652a308079152d5dd2549a0c1c08a628c
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-04-03 18:54:28 -0400

    Created assignment 16

---
+++ Project_Files/assignment16/app/src/main/java/com/example/application16/RecyclerAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.application16
+
+import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter(myViewModel: MainViewModel) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    private var viewModel : MainViewModel = myViewModel
+
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage : ImageView
+        var itemTitle : TextView
+        var itemDetail : TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.itemImage)
+            itemTitle = itemView.findViewById(R.id.itemTitle)
+            itemDetail = itemView.findViewById(R.id.itemDetail)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup : ViewGroup, i: Int) : ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i : Int) {
+        viewHolder.itemTitle.text = viewModel.getItemTitle(i)
+        viewHolder.itemDetail.text = viewModel.getItemDetail(i)
+        viewHolder.itemImage.setImageResource(viewModel.getItemImage(i))
+
+//        viewHolder.itemTitle.text = titles[i]
+//        viewHolder.itemDetail.text = details[i]
+//        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount() : Int {
+        return viewModel.getListSize()
+    }
+}
